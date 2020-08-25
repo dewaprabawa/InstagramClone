70253f7109ef4a7f34dda40754345617ed3e3c73
dewa <dewaprabawa>
2020-08-25 18:42:49 +0800

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Dewa Prabawa on 25/08/20.
+//  Copyright © 2020 Dewa Prabawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
